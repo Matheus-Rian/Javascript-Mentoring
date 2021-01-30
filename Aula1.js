f3268a703073ab8acc209205e9b79932270193df
Matheus Rian <souzamatheusrian@gmail.com>
2021-01-29 23:28:57 -0200

    feat: Aula 1

---
+++ Aula1.js
@@ -0,0 +1,38 @@
+// Algoritmo
+// Conjunto de passos finitos para resolver um problema
+
+// Variaveis
+// É uma estrutura para armazenar valor(Espaço na memória)
+
+// O que podemos armazenar nas variaveis
+
+// 1 - String(' ') - texto e palavras.
+// 2 - Number - Números
+// 3 - Boolean - True ou false
+// 4 - Array - Sequência de valores
+// 5 - Objeto - Um conjunto de várias variaveis
+
+var string = 'fogão'
+//string
+console.log(cozinha)
+
+let number = 1.2
+//number
+console.log(number)
+
+let boolean = true
+
+console.log(boolean)
+
+let array = ['kaio', 1.2, boolean]
+
+console.log(array)
+
+let objeto = {
+	cozinha: 'geladeira',
+	number1: 1111111111111,
+	boolean1: false,
+	array: ['matheus', 409998, boolean, number]
+}
+
+console.log(objeto)
